5e8267148d2a158991c1dca9622544ddb3117a53
hong.liang <296787250@qq.com>
2022-11-26 21:32:43 +0800

    get请求

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,5 +1,28 @@
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class TestDemo {
-    public static void main(String[] args) {
-        System.out.println("测试代码");
+    public String run(OkHttpClient client, String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+    public static void main( String[] args )
+    {
+        OkHttpClient client = new OkHttpClient();
+        try
+        {
+            String res = new TestDemo().run(client, "https://home.cnblogs.com/u/hong-fithing");
+            System.out.println(res);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
