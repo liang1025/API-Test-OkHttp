958a9f928f12164bcfe4dbe0b82c6a27360fb1c0
hong.liang <296787250@qq.com>
2022-11-27 21:31:18 +0800

    更新pom

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,6 +1,6 @@
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.IOException;
 
